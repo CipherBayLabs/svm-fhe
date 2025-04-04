3fea80470acc0142c4ed57818852698e7b2d5836
Kyle Koshiyama <43145199+kkoshiya@users.noreply.github.com>
2025-04-03 23:36:38 -0700

    withdraw function on program

--- blockchain/programs/blockchain/src/lib.rs
+++ blockchain/programs/blockchain/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use solana_program::hash::Hash;
 
 declare_id!("GEFoAn6CNJiG9dq8xgm24fjzjip7n5GcH5AyqVC6QzdD");
 
@@ -54,8 +55,7 @@ pub mod blockchain {
         msg!("FHE Add - LHS: {:?}", lhs);
         msg!("FHE Add - RHS: {:?}", rhs);
         
-        let clock = Clock::get()?;
-        let result_value = generate_ciphertext(&clock);
+        let result_value = Hash::new_unique().to_bytes();
         
         ctx.accounts.result_info.owner = ctx.accounts.user.key();
         ctx.accounts.result_info.value = result_value;
@@ -89,6 +89,31 @@ pub mod blockchain {
         ctx.accounts.result_info.value = value;
         
         msg!("Encrypted result stored");
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, recipient: Pubkey) -> Result<()> {
+        // Check owner
+        require!(
+            ctx.accounts.owner.key() == ctx.accounts.program_data.upgrade_authority_address.unwrap(),
+            ProgramError::IncorrectProgramId
+        );
+
+        let recipient_starting_lamports = ctx.accounts.recipient.lamports();
+        **ctx.accounts.recipient.lamports.borrow_mut() = recipient_starting_lamports
+            .checked_add(amount)
+            .unwrap();
+
+        let vault_lamports = ctx.accounts.vault.lamports();
+        **ctx.accounts.vault.lamports.borrow_mut() = vault_lamports
+            .checked_sub(amount)
+            .unwrap();
+
+        msg!("Owner {} withdrew {} lamports to {}", 
+            ctx.accounts.owner.key(),
+            amount,
+            recipient
+        );
         Ok(())
     }
 }
@@ -179,4 +204,19 @@ pub struct FHEOperation<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub vault: SystemAccount<'info>,
+    
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(mut)]
+    /// CHECK: Can be any account, owner decides
+    pub recipient: UncheckedAccount<'info>,
+    
+    pub program_data: Account<'info, ProgramData>,
 }
